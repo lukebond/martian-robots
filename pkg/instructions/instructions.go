9d0d5f98d641735eef0c76f7c23fefa291ff9849
Luke Bond <luke.n.bond@gmail.com>
2019-12-09 15:37:20 +0000

    added instruction data loading and processing
    one failing test due to the fact that robot's looking for grid scents isn't yet
    implemented.

--- pkg/instructions/instructions.go
+++ pkg/instructions/instructions.go
@@ -3,10 +3,12 @@ package instructions
 import (
   "bufio"
   "errors"
+  "fmt"
   "os"
   "strconv"
   "strings"
 
+  "github.com/lukebond/martian-robots/pkg/grid"
   "github.com/lukebond/martian-robots/pkg/robot"
 )
 
@@ -70,4 +72,45 @@ func LoadInstructionsFromFile(filename string) (*Instructions, error) {
 
 	file.Close()
   return &i, nil
+}
+
+func (i *Instructions) ProcessInstructions() {
+  g := grid.NewGrid(i.MaxX + 1, i.MaxY + 1)
+  for r := 0; r < len(i.Robots); r++ {
+    ProcessSequence(&i.Robots[r], i.Sequences[r], g)
+  }
+}
+
+func ProcessSequence(r *robot.Robot, seq string, g *grid.Grid) string {
+  var lost = false
+  for s := 0; s < len(seq); s++ {
+    if lost {
+      break
+    }
+    instruction := seq[s]
+    switch instruction {
+    case 'L':
+      r.RotateLeft()
+    case 'R':
+      r.RotateRight()
+    case 'F':
+      lastX := r.X
+      lastY := r.Y
+      r.Forward()
+      if r.X < 0 || r.X >= g.Width || r.Y < 0 || r.Y >= g.Height {
+        fmt.Printf("Robot lost at %d,%d. Last position: %d,%d\n", r.X, r.Y, lastX, lastY)
+        g.SetScent(lastX, lastY)
+        r.X = lastX
+        r.Y = lastY
+        lost = true
+      }
+    default:
+      fmt.Printf("Ignoring instruction '%v'", instruction)
+    }
+  }
+  var output = fmt.Sprintf("%d %d %v", r.X, r.Y, r.Orientation)
+  if lost {
+    output = output + " LOST"
+  }
+  return output
 }
--- pkg/instructions/instructions_test.go
+++ pkg/instructions/instructions_test.go
@@ -4,7 +4,9 @@ import (
   "fmt"
   "testing"
 
+  "github.com/lukebond/martian-robots/pkg/grid"
   "github.com/lukebond/martian-robots/pkg/instructions"
+  "github.com/lukebond/martian-robots/pkg/robot"
 )
 
 func TestLoadInstructions(t *testing.T) {
@@ -22,5 +24,32 @@ func TestLoadInstructions(t *testing.T) {
   }
   if i.Robots[2].X != 0 || i.Robots[2].Y != 3 || i.Robots[2].Orientation != "W" {
     t.Errorf("Loading instructions resulted in incorrect robot position & orientation")
+  }
+}
+
+func TestProcessSequence1(t *testing.T) {
+  g := grid.NewGrid(6, 4)
+  r := robot.Robot{X: 1, Y: 1, Orientation: "E"}
+  output := instructions.ProcessSequence(&r, "RFRFRFRF", g)
+  if output != "1 1 E" {
+    t.Errorf("Test sequence output '%v' didn't match sample data", output)
+  }
+}
+
+func TestProcessSequence2(t *testing.T) {
+  g := grid.NewGrid(6, 4)
+  r := robot.Robot{X: 3, Y: 2, Orientation: "N"}
+  output := instructions.ProcessSequence(&r, "FRRFLLFFRRFLL", g)
+  if output != "3 3 N LOST" {
+    t.Errorf("Test sequence output '%v' didn't match sample data", output)
+  }
+}
+
+func TestProcessSequence3(t *testing.T) {
+  g := grid.NewGrid(6, 4)
+  r := robot.Robot{X: 0, Y: 3, Orientation: "W"}
+  output := instructions.ProcessSequence(&r, "LLFFFLFLFL", g)
+  if output != "2 3 S" {
+    t.Errorf("Test sequence output '%v' didn't match sample data", output)
   }
 }
